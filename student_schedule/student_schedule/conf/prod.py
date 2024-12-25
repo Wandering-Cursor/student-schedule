0a6a08bc96f8f5e0fe5232fdf7bb4b3eaa89223c
Pavlo Pohorieltsev <49622129+makisukurisu@users.noreply.github.com>
2024-12-25 23:41:16 +0200

    fix: Add secrets properly; Fix port; Add static urls

--- student_schedule/student_schedule/conf/prod.py
+++ student_schedule/student_schedule/conf/prod.py
@@ -9,3 +9,6 @@ LOGGING["handlers"]["console"]["level"] = "WARNING"  # noqa: F405
 
 CORS_ALLOWED_ORIGINS = settings.allowed_hosts
 CSRF_TRUSTED_ORIGINS = settings.trusted_origins
+
+STATIC_URL = "/schedule/static/"
+MEDIA_URL = "/schedule/media/"
