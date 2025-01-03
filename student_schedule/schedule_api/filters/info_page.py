372150d59bff076873fd26833ca53376c5de1cf3
Pavlo Pohorieltsev <49622129+makisukurisu@users.noreply.github.com>
2025-01-03 15:37:44 +0000

    Update filters for InfoPage

--- student_schedule/schedule_api/filters/info_page.py
+++ student_schedule/schedule_api/filters/info_page.py
@@ -4,9 +4,10 @@ from schedule_admin.models.info_pages.tags import Tag
 
 
 class InfoPageFilter(django_filters.FilterSet):
-    tags = django_filters.ModelChoiceFilter(
+    tags = django_filters.ModelMultipleChoiceFilter(
         to_field_name="uuid",
         queryset=Tag.objects.all(),
+        conjoined=True,
     )
     title = django_filters.CharFilter(
         lookup_expr="icontains",
