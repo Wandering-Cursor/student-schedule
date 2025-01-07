7e2465b2fa0ecb19af4d211b7a7d632e22eb3e65
Pavlo Pohorieltsev <49622129+makisukurisu@users.noreply.github.com>
2025-01-07 19:43:42 +0000

    fix: Update combined schedule

--- frontend/src/components/schedule/CombinedScheduleItem.vue
+++ frontend/src/components/schedule/CombinedScheduleItem.vue
@@ -6,6 +6,7 @@ import { getLocalDateFromString } from '@/utils/datetime'
 import GroupLink from '@/components/groups/GroupLink.vue'
 import Variant from '@/enums/Variant'
 import { getLastPart } from '@/utils/urls'
+import AnyLink from '../common/AnyLink.vue'
 
 const props = defineProps({
   schedule: {
@@ -21,33 +22,36 @@ const props = defineProps({
       {{ getLocalDateFromString(schedule.for_date) }}
     </template>
     <template #content>
-      <RouterLink
-        :to="`/schedule/photo/${getLastPart(schedule.photo_schedule)}`"
-        v-if="schedule.photo_schedule"
-      >
-        <h3>{{ $t('schedule.photo.title') }}</h3>
-      </RouterLink>
-      <div class="group-schedule" v-if="schedule.group_schedules">
-        {{ $t('schedule.group.title') }}
-        <div class="group-schedule-container">
-          <Card v-for="item in schedule.group_schedules" :key="schedule.uuid">
-            <template #content>
-              <GroupLink
-                :group-i-d="item.group.uuid"
-                :group-name="item.group.name"
-                :variant="Variant.P"
-                class="group-schedule-item-content"
-              />
-            </template>
-            <template #footer>
-              <Button
-                as="router-link"
-                :label="$t('labels.view')"
-                :to="`/schedule/group/${item.uuid}`"
-                class="group-schedule-item-content"
-              />
-            </template>
-          </Card>
+      <div class="contained-wrapper schedule-item">
+        <AnyLink
+          v-if="schedule.photo_schedule"
+          :id-field="getLastPart(schedule.photo_schedule)"
+          :name-field="$t('schedule.photo.title')"
+          linkTo="photo-schedule"
+          :variant="Variant.H3"
+        />
+        <div class="group-schedule" v-if="schedule.group_schedules.length > 0">
+          {{ $t('schedule.group.title') }}
+          <div class="group-schedule-container">
+            <Card v-for="item in schedule.group_schedules" :key="schedule.uuid">
+              <template #content>
+                <GroupLink
+                  :group-i-d="item.group.uuid"
+                  :group-name="item.group.name"
+                  :variant="Variant.P"
+                  class="group-schedule-item-content"
+                />
+              </template>
+              <template #footer>
+                <Button
+                  as="router-link"
+                  :label="$t('labels.view')"
+                  :to="`/schedule/group/${item.uuid}`"
+                  class="group-schedule-item-content"
+                />
+              </template>
+            </Card>
+          </div>
         </div>
       </div>
     </template>
@@ -62,5 +66,11 @@ const props = defineProps({
 }
 .group-schedule-item-content {
   width: 100%;
+}
+.group-schedule {
+  width: 100%;
+}
+.schedule-item {
+  align-items: start;
 }
 </style>
