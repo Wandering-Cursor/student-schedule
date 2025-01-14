01aed2818659f58d428329a18ad3f854ed4f9fa1
Pavlo Pohorieltsev <49622129+makisukurisu@users.noreply.github.com>
2025-01-14 16:11:03 +0000

    Allow all origins

--- student_schedule/student_schedule/conf/prod.py
+++ student_schedule/student_schedule/conf/prod.py
@@ -12,8 +12,8 @@ SESSION_COOKIE_SECURE = True
 
 LOGGING["handlers"]["console"]["level"] = "WARNING"
 ALLOWED_HOSTS = settings.allowed_hosts
-CORS_ALLOWED_ORIGINS = settings.allowed_cors_origins
 CSRF_TRUSTED_ORIGINS = settings.trusted_origins
+CORS_ALLOW_ALL_ORIGINS = True
 
 STATIC_URL = "/schedule/static/"
 MEDIA_URL = "/schedule/media/"
